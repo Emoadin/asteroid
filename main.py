b400b684ae9ac6c257ce6c64a5f9451a0f2476bc
Emoadin <trent.stallan@gmail.com>
2024-08-23 09:49:59 +1000

    finished

--- main.py
+++ main.py
@@ -1,23 +1,66 @@
+import sys
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+from shot import Shot
 
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    Asteroid.containers = (updatable, drawable, asteroids)  
+    Player.containers = (updatable, drawable)
+
+    asteroid_field = AsteroidField()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))  # this had to come after Player.containers so that it knows about the groups
+
+    
+
     dt = 0
+    
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        player.draw(screen)
         
+        
+        #player.draw(screen)
+        
+        #player.update(dt)
+        screen.fill((0,0,0))
+        
+
+        
+
+        for obj in drawable:
+            obj.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in asteroids:
+            if player.collision(obj):
+                print("GAME OVER!!!")                
+                sys.exit()
+        for obj in asteroids:
+            for shot in shots:
+                if shot.collision(obj):
+                    shot.kill()
+                    obj.split()
+
+
         pygame.display.flip()
         
 
